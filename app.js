e539dd937c5557ae90019f786a760f7845c14c8c
Mariah <mariahschock@gmail.com>
2022-05-17 11:11:18 -0700

    linking create button to create page, adding html and css

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 // import functions and grab DOM elements
 const authBtn = document.getElementById('auth-button');
+const createBtn = document.getElementById('create-button');
 // let state
 
 // set event listeners 
 authBtn.addEventListener('click', async (e) => {
     e.preventDefault();
-    return location.replace('/Auth-Page');
+    return location.assign('/Auth-Page');
 });
+
+createBtn.addEventListener('click', async (e) => {
+    e.preventDefault();
+    return location.assign('/Create-Page');
+});
+
+
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
